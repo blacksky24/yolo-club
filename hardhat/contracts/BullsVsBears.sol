cd30e88c8f40c2e2e87bbb0ae4a6d10188fbeae5
Shubham0850 <rajshubham0850@gmail.com>
2022-07-02 23:06:34 +0530

    new contract

--- hardhat/contracts/BullsVsBears.sol
+++ hardhat/contracts/BullsVsBears.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract BullsVsBears {
+contract BullBear is Ownable {
     using SafeMath for uint;
 
     // State Varibales
@@ -26,7 +27,7 @@ contract BullsVsBears {
     uint256 public noOfBullTickets;
     uint256 public noOfBearTickets;
 
-    uint256 perTicketReturn;
+    uint256 public perTicketReturn;
 
     enum Winner{TBD, BULLS, BEARS, DRAW}
     Winner public poolWinner;
@@ -52,37 +53,59 @@ contract BullsVsBears {
         priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
     }
 
-    // functions
-
-    modifier saleStarted(){
-        require(block.timestamp >= bettingPoolStartTime, "sale is not started yet");
-        require(block.timestamp <= bettingPoolEndTime, "sale is not started yet");
+    modifier activePool(){
+        require(block.timestamp >= bettingPoolStartTime, "Sale is not started yet");
+        require(block.timestamp <= bettingPoolEndTime, "Betting pool is closed");
         _;
+    }
+
+    function updatePoolStatus(_bettingPoolStatus) public onlyOwner {
+        bettingPoolStatus = _bettingPoolStatus;
+
+        if(bettingPoolStatus == PoolStatus.ACTIVE){
+            // when pool status is active
+        }
+        else if(bettingPoolStatus == PoolStatus.WAITING){
+            //
+        }
+        else if(bettingPoolStatus == PoolStatus.CLAIM){
+            // step1: Transfer the commission to the owner/admin
+            transferCommission();
+
+            // step:2 Pick who won the Pool
+            poolWinner = pickWinner();
+
+            // step3: calculate Per Ticket Winning Prize
+            perTicketReturn = calculateWinningPrice();
+        }
+        else if(bettingPoolStatus == PoolStatus.COMPLETED){
+            //
+        }
     }
 
     function getPoolBalance() public view returns(uint) {
         return address(this).balance;
     }
 
-    function buyBullTicket() payable public saleStarted returns(bool){
-        require(msg.value == ticketPrice, "Please pay valid ticket price");
-        bulls[msg.sender] += 1;
+    function buyBullTicket(uint8 _noOfTickets) payable public activePool returns(bool){
+        require(msg.value == mul(_noOfTickets, ticketPrice), "Please pay valid ticket price");
+        bulls[msg.sender] += _noOfTickets;
 
-        noOfBullTickets += 1;
-
-        return true;
-    }
-
-    function buyBearTicket() payable public saleStarted returns(bool){
-        require(msg.value == ticketPrice, "Please pay valid ticket price");
-        bears[msg.sender] += 1;
-
-        noOfBearTickets += 1;
+        noOfBullTickets += _noOfTickets;
 
         return true;
     }
 
-    function getLatestPrice() public view returns (int) {
+    function buyBearTicket(uint8 _noOfTickets) payable public activePool returns(bool){
+       require(msg.value == mul(_noOfTickets, ticketPrice), "Please pay valid ticket price");
+        bears[msg.sender] += _noOfTickets;
+
+        noOfBearTickets += _noOfTickets;
+
+        return true;
+    }
+
+    function getLatestPrice() internal view returns (int) {
     (
         uint80 roundID, 
         int price,
@@ -93,50 +116,58 @@ contract BullsVsBears {
         return price;
     }
 
-    function pickWinner() public returns(string){
-        require(block.timestamp == )
+    function pickWinner() internal returns(string){
+        require(bettingPoolStatus == PoolStatus.CLAIM, "Not time to pick the winner");
+
         uint256 currentPrice = getLatestPrice();
 
         if(bettingPrice > currentPrice){
             poolWinner = Winner.BEARS;
-            return "BEARS"
+            return "BEARS";
         } 
         else if(bettingPrice < currentPrice){
             poolWinner = Winner.BULLS;
-            return "BULLS"
+            return "BULLS";
         }
         else{
             poolWinner = Winner.DRAW;
-            return "DRAW"
+            return "DRAW";
         }
     }
 
-    function calculateWinningPrice() public return(uint256){
+    function transferCommission() internal payable {
+        uint256 commission = address(this).balance.div(20);
+
+        payable(owner()).transfer(commision);
+    }
+
+    function calculateWinningPrice() internal return(uint256){
         // 5% commision
-        uint256 commision = address(this).balance.div(20);
+        uint256 commission = address(this).balance.div(20);
 
         // price to be disctributed
-        uint256 priceToDistribute = address(this).balance - commision;
+        uint256 priceToDistribute = address(this).balance - commission;
 
         // Bull Wins
         if(poolWinner == Winner.BULLS){  
             // per ticket return
-            perTicketReturn = priceToDistribute / noOfBullTickets;
+            return priceToDistribute / noOfBullTickets;
         }
         else if(poolWinner == Winner.BEARS){
             // per ticket return
-            perTicketReturn = priceToDistribute / noOfBearTickets;
+            return priceToDistribute / noOfBearTickets;
         } 
         else if(poolWinner == Winner.DRAW){
             uint256 totalWinner = add(noOfBearTickets, noOfBullTickets);
             // per ticket return
-            perTicketReturn = priceToDistribute / totalWinner;
+            return priceToDistribute / totalWinner;
         }
 
-        return perTicketReturn;
     }
 
     function claimWinning() public payable {
+        require(bettingPoolStatus == PoolStatus.CLAIM, "Please wait for the betting end date");
+
         if(poolWinner == Winner.BULLS){
             require(bulls[msg.sender] != 0, "Only Bulls ticket owner call this");
             
@@ -165,9 +196,9 @@ contract BullsVsBears {
             uint noOfBull = bulls[msg.sender];
             uint noOfBear = bears[msg.sender];
 
-            uint totalTicket = add(noOfBear, noOfBull);
+            uint totalUserTickets = add(noOfBear, noOfBull);
 
-            uint256 amountToPay = mul(totalWinner, perTicketReturn);
+            uint256 amountToPay = mul(totalUserTickets, perTicketReturn);
 
             bears[msg.sender] = 0;
             bulls[msg.sender] = 0;
@@ -175,7 +206,6 @@ contract BullsVsBears {
             payable(msg.sender).transfer(amountToPay);
         }
     }
-
 }
 
 // On 30 JUne, 12:00:00 PM GMT, price of ETH will be Greater than or equal to 1200
